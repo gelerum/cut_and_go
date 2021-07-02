b3f71fe37d74671cb7647800562107d63ad2e86a
Gleb Gonchar <codegleb@gmail.com>
2021-07-02 14:19:02 +0700

    remove dotenv support

--- cut_and_go/settings.py
+++ cut_and_go/settings.py
@@ -1,12 +1,8 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-dotenv_path = os.path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
